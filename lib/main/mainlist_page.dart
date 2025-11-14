c94f003dc1be92475b9cc1af6d1a445e7c0886a7
938938 <938kjh@gmail.com>
2025-11-14 15:01:17 +0900

    [FEAT] 초기 설정

---
+++ lib/main/mainlist_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MainPage();
+  }
+}
+
+class _MainPage extends State<MainPage>{
+  @override
+  Widget build(BuildContext conext){
+    return Scaffold();
+  }
+}
